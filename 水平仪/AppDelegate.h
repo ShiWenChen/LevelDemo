986d9f87d8142a857dc8af6ce6de7a8210b7791f
ShiWenChen <cswzmg@live.com>
2016-10-13 11:25:16 +0800

    Initial Commit

---
+++ 水平仪/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  水平仪
+//
+//  Created by admin on 2016/10/13.
+//  Copyright © 2016年 racer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 水平仪/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  水平仪
+//
+//  Created by admin on 2016/10/13.
+//  Copyright © 2016年 racer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
